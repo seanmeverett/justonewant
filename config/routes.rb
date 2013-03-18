8e503dff16b9456f0c0c62de6a567b5330e798ba
Sean Everett <seanmeverett@gmail.com>
2013-03-17 21:03:34 -0500

    Updated profile page and added scoped status updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Justonewant::Application.routes.draw do
 
   resources :statuses
   get 'feed', to: 'statuses#index', as: :feed
-  
   root :to => 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
