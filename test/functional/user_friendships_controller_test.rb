cb6aba742c38de3c331af7e21037a8f8dce4da9a
Sean Everett <seanmeverett@gmail.com>
2013-03-24 18:15:59 -0500

    adding Ajax to Friends page to show who's friending who

--- test/functional/user_friendships_controller_test.rb
+++ test/functional/user_friendships_controller_test.rb
@@ -215,7 +215,7 @@ class UserFriendshipsControllerTest < ActionController::TestCase
       setup do
         @user_friendship = create(:pending_user_friendship, user: users(:sean))
         sign_in users(:sean)
-        get :edit, id: @user_friendship
+        get :edit, id: @user_friendship.friend.profile_name
       end
 
       should "get edit and return success" do
