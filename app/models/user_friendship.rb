ed0a23505ac52c0edb248cdbb7a0eb8053bc0baf
Sean Everett <seanmeverett@gmail.com>
2013-03-24 15:32:30 -0500

    updated user_friendship to include accepting mutual friendships

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -5,7 +5,7 @@ class UserFriendship < ActiveRecord::Base
   attr_accessible :user, :friend, :user_id, :friend_id, :state
 
   state_machine :state, initial: :pending do
-  	after_transition on: :accept, do: :send_acceptance_email
+  	after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship!]
 
   	state :requested
 
@@ -30,5 +30,15 @@ class UserFriendship < ActiveRecord::Base
 
   def send_acceptance_email
   	UserNotifier.friend_request_accepted(id).deliver
+  end
+
+  def mutual_friendship
+    self.class.where({user_id: friend_id, friend_id: user_id}).first
+  end
+
+  def accept_mutual_friendship!
+    # Grab the mutual friendship and update the state without using
+    # the state machine so as not to invoke callbacks.
+    mutual_friendship.update_attribute(:state, 'accepted')
   end
 end
--- test/unit/user_friendship_test.rb
+++ test/unit/user_friendship_test.rb
@@ -37,9 +37,36 @@ class UserFriendshipTest < ActiveSupport::TestCase
     end
   end
 
+  context "#mutual_friendship" do
+    setup do
+      UserFriendship.request users(:sean), users(:tragnark)
+      @friendship1 = users(:sean).user_friendships.where(friend_id: users(:tragnark).id).first
+      @friendship2 = users(:tragnark).user_friendships.where(friend_id: users(:sean).id).first
+    end
+
+    should "correctly find the mutual friendship" do
+      assert_equal @friendship2, @friendship1.mutual_friendship
+    end
+  end
+
+  context "#accept_mutual_friendship!" do
+    setup do
+      UserFriendship.request users(:sean), users(:tragnark)
+    end
+
+    should "accept the mutual friendship" do
+      friendship1 = users(:sean).user_friendships.where(friend_id: users(:tragnark).id).first
+      friendship2 = users(:tragnark).user_friendships.where(friend_id: users(:sean).id).first
+      
+      friendship1.accept_mutual_friendship!
+      friendship2.reload
+      assert_equal 'accepted', friendship2.state
+    end
+  end
+
   context "#accept!" do
     setup do
-      @user_friendship = UserFriendship.create user: users(:sean), friend: users(:mike)
+      @user_friendship = UserFriendship.request users(:sean), users(:mike)
     end
 
     should "set the state to accepted" do
@@ -57,6 +84,11 @@ class UserFriendshipTest < ActiveSupport::TestCase
       @user_friendship.accept!
       users(:sean).friends.reload
       assert users(:sean).friends.include?(users(:mike))
+    end
+
+    should "accept the mutual friendship" do
+      @user_friendship.accept!
+      assert_equal 'accepted', @user_friendship.mutual_friendship.state
     end
   end
 
